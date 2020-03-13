71a44dbbf06949b26acf16b3ff4c9644b7565767
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-13 14:55:39 -0700

    Consolidate ConversionPage

--- Source/XamConverter/Pages/ConversionPage.cs
+++ Source/XamConverter/Pages/ConversionPage.cs
@@ -1,81 +1,87 @@
-﻿using Xamarin.Forms;
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xamarin.Forms.Markup;
 using static Xamarin.Forms.Markup.GridRowsColumns;
 using static XamConverter.Views.MarkupExtensions;
 
 namespace XamConverter
 {
-    partial class ConversionPage
+    class ConversionPage : BaseContentPage<ConversionViewModel>
     {
-        enum Row { UnitType, NumberToConvert, OriginalUnits, ConvertedUnits, ConvertedNumber, ConvertButton };
-        enum Column { Label, Input };
-        
-        void Build()
+        public ConversionPage()
         {
-            var vm = ViewModel;
             BackgroundColor = ColorConstants.LightPurple;
-            this.Bind(nameof(vm.TitleText));
+            ViewModel.ConversionError += HandleConversionError;
 
-            Content = new Grid {
+            this.Bind(nameof(ConversionViewModel.TitleText));
+
+            Content = new Grid
+            {
                 ColumnSpacing = 20,
 
-                RowDefinitions = Rows.Define (
-                    (Row.UnitType       , Star),
+                RowDefinitions = Rows.Define(
+                    (Row.UnitType, Star),
                     (Row.NumberToConvert, Star),
-                    (Row.OriginalUnits  , Star),
-                    (Row.ConvertedUnits , Star),
+                    (Row.OriginalUnits, Star),
+                    (Row.ConvertedUnits, Star),
                     (Row.ConvertedNumber, Star),
-                    (Row.ConvertButton  , Stars(2))),
+                    (Row.ConvertButton, Stars(2))),
 
-                ColumnDefinitions = Columns.Define (
+                ColumnDefinitions = Columns.Define(
                     (Column.Label, Star),
                     (Column.Input, Star)),
 
                 Children = {
-                    new DarkPurpleLabel ("Unit Type")
-                        .Row (Row.UnitType) .Column (Column.Label),
+                    new DarkPurpleLabel("Unit Type")
+                       .Row(Row.UnitType).Column(Column.Label),
 
-                    new UnitsPicker ("Unit Type")
-                        .Row (Row.UnitType) .Column (Column.Input)
-                        .Bind (Picker.ItemsSourceProperty, nameof(vm.UnitTypePickerList))
-                        .Bind (UnitsPicker.SelectedIndexChangedCommandProperty, nameof(vm.UnitTypePickerSelectedIndexChangedCommand))
-                        .Bind (nameof(vm.UnitTypePickerSelectedIndex)),
+                    new UnitsPicker("Unit Type")
+                       .Row(Row.UnitType).Column(Column.Input)
+                       .Bind(Picker.ItemsSourceProperty, nameof(ConversionViewModel.UnitTypePickerList))
+                       .Bind(UnitsPicker.SelectedIndexChangedCommandProperty, nameof(ConversionViewModel.UnitTypePickerSelectedIndexChangedCommand))
+                       .Bind(nameof(ConversionViewModel.UnitTypePickerSelectedIndex)),
 
-                    new DarkPurpleLabel ("Number to Convert")
-                        .Row (Row.NumberToConvert) .Column (Column.Label),
+                    new DarkPurpleLabel("Number to Convert")
+                       .Row(Row.NumberToConvert).Column(Column.Label),
 
-                    new NumberToConvertEntry ()
-                        .Row (Row.NumberToConvert) .Column (Column.Input)
-                        .Bind (nameof(vm.NumberToConvertEntryText)),
+                    new NumberToConvertEntry()
+                       .Row(Row.NumberToConvert).Column(Column.Input)
+                       .Bind(nameof(ConversionViewModel.NumberToConvertEntryText)),
 
-                    new DarkPurpleLabel ("Original Units")
-                        .Row (Row.OriginalUnits) .Column (Column.Label),
+                    new DarkPurpleLabel("Original Units")
+                       .Row(Row.OriginalUnits).Column(Column.Label),
 
-                    new UnitsPicker ("Original Units")
-                        .Row (Row.OriginalUnits) .Column (Column.Input)
-                        .Bind (Picker.ItemsSourceProperty, nameof(vm.OriginalUnitsPickerList))
-                        .Bind (UnitsPicker.SelectedIndexChangedCommandProperty, nameof(vm.OriginalUnitsPickerSelectedIndexChangedCommand))
-                        .Bind (Picker.SelectedItemProperty, nameof(vm.OriginalUnitsPickerSelectedItem)),
+                    new UnitsPicker("Original Units")
+                       .Row(Row.OriginalUnits).Column(Column.Input)
+                       .Bind(Picker.ItemsSourceProperty, nameof(ConversionViewModel.OriginalUnitsPickerList))
+                       .Bind(UnitsPicker.SelectedIndexChangedCommandProperty, nameof(ConversionViewModel.OriginalUnitsPickerSelectedIndexChangedCommand))
+                       .Bind(Picker.SelectedItemProperty, nameof(ConversionViewModel.OriginalUnitsPickerSelectedItem)),
 
-                    new DarkPurpleLabel ("Converted Units")
-                        .Row (Row.ConvertedUnits) .Column (Column.Label),
+                    new DarkPurpleLabel("Converted Units")
+                       .Row(Row.ConvertedUnits).Column(Column.Label),
 
-                    new UnitsPicker ("Converted Units")
-                        .Row (Row.ConvertedUnits) .Column (Column.Input)
-                        .Bind (Picker.ItemsSourceProperty, nameof(vm.ConvertedUnitsPickerList))
-                        .Bind (UnitsPicker.SelectedIndexChangedCommandProperty, nameof(vm.ConvertedUnitsPickerSelectedIndexChangedCommand))
-                        .Bind (Picker.SelectedItemProperty, nameof(vm.ConvertedUnitsPickerSelectedItem)),
+                    new UnitsPicker("Converted Units")
+                       .Row(Row.ConvertedUnits).Column(Column.Input)
+                       .Bind(Picker.ItemsSourceProperty, nameof(ConversionViewModel.ConvertedUnitsPickerList))
+                       .Bind(UnitsPicker.SelectedIndexChangedCommandProperty, nameof(ConversionViewModel.ConvertedUnitsPickerSelectedIndexChangedCommand))
+                       .Bind(Picker.SelectedItemProperty, nameof(ConversionViewModel.ConvertedUnitsPickerSelectedItem)),
 
-                    new DarkPurpleLabel ("")
-                        .Row (Row.ConvertedNumber) .ColumnSpan (All<Column>()) .TextCenterHorizontal ()
-                        .Bind (nameof(vm.ConvertedNumberLabelText)),
+                    new DarkPurpleLabel("")
+                       .Row(Row.ConvertedNumber).ColumnSpan(All<Column>()).TextCenterHorizontal()
+                       .Bind(nameof(ConversionViewModel.ConvertedNumberLabelText)),
 
-                    new ConvertButton ()
-                        .Row (Row.ConvertButton) .ColumnSpan (All<Column>()) .FillExpandHorizontal()
-                        .Bind (nameof(vm.ConvertButtonCommand))
+                    new ConvertButton()
+                       .Row(Row.ConvertButton).ColumnSpan(All<Column>()).FillExpandHorizontal()
+                       .Bind(nameof(ConversionViewModel.ConvertButtonCommand))
                 }
-            } .Center ();
+            }.Center();
         }
+
+        enum Row { UnitType, NumberToConvert, OriginalUnits, ConvertedUnits, ConvertedNumber, ConvertButton };
+        enum Column { Label, Input };
+
+        void HandleConversionError(object sender, string message) =>
+            MainThread.BeginInvokeOnMainThread(async () => await DisplayAlert("Conversion Error", message, "OK"));
 
         class NumberToConvertEntry : Entry
         {
@@ -88,7 +94,7 @@ namespace XamConverter
             }
         }
 
-        class ConvertButton : Button
+        class ConvertButton : BounceButton
         {
             public ConvertButton()
             {
