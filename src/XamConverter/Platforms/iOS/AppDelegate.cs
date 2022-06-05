46d3218e029d508c2971412c97a2c978f4fd0ed2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-06-04 21:51:28 -0700

    Set Xcode Version

--- src/XamConverter/Platforms/iOS/AppDelegate.cs
+++ src/XamConverter/Platforms/iOS/AppDelegate.cs
@@ -2,7 +2,7 @@
 
 namespace XamConverter;
 
-[Register(nameof(AppDelegate)]
+[Register(nameof(AppDelegate))]
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
